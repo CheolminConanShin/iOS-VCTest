ea1961845bcec356a6376b8fc0fc4258b5fd7268
SDS_110 <sds_110@70-12-216-151.pools.spcsdns.net>
2016-02-24 14:52:33 +0900

    Initial Commit

---
+++ VCTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VCTest
+//
+//  Created by SDS_110 on 2016. 2. 24..
+//  Copyright © 2016년 SDS_110. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
